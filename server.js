c180cff47b25d4c913ae19df3c32080d56ffee86
Luis Sanchez <luis.921451@gmail.com>
2016-07-07 09:07:06 -0500

    Lab04 IP-PORT Server con IP y puerto tomados del entorno

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 //http
 var http = require("http");
+//obteniendo informacion del entorno de ejecucion con respecto al IP
+//y al puerto que debeomos uzar en nuestro server.
+var PORT = process.env.PORT || 3000;
+var IP = process.env.IP || '127.0.0.1';
+if (IP == '127.0.0.1')
+{
+    console.log(">--------------Ejecutando en modo Local ------------");
+}
 //crear un servidor basico
 //cargar un programa a nuestro servidor
 var server = http.createServer(function (req,res) {
@@ -10,12 +18,12 @@ var server = http.createServer(function (req,res) {
         "Server": "ITGAM@4.4.7"
     });
     //Enviamor la respuesta
-    res.write("Hola Mundo");
+    res.write("Hola Luis Enrique Sanchez");
     //Cerra la conexion
     res.end();
 });
 //Poner a trabajar al server
 //usar un puerto y la direccion ip o locall host
-server.listen(3000,"127.0.0.1",function () {
-    console.log("Server listening @http://localhost:3000.....");
+server.listen(PORT,IP,function () {
+    console.log(`> Server listening @http://${IP}:${PORT}.....`);
 });
