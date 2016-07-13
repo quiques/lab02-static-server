f3bb58af0d4b44a3e0f3c11ae134b013b09d6c4e
Luis Sanchez <luis.921451@gmail.com>
2016-07-13 08:07:11 -0500

    LAB09seweetalert

--- static/js/site.js
+++ static/js/site.js
@@ -1,12 +1,13 @@
 function hola() {
-    alert("Bienvenido a esta pagina...")
+    sweetAlert("World Places","¡Bienvenido a esta pagina!");
+    document.body.style.backgroundColor = "lightseagreen"
 }
 function changeBkgColor(element,color) {
     var color = document.body.style.backgroundColor;
     if(color == "lightseagreen"){
         color = "honeydew"
     }else{
-        color == "lightseagreen";
+        color = "lightseagreen";
     }
     console.log(">Cambiando el color a   "+ color);
     document.body.style.backgroundColor = color;
